7b9aba54d7be10e5bedd3e9d8833f2e62b21da1a
Keks <keks@htmlacademy.ru>
2024-03-21 16:35:34 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/function.js
@@ -0,0 +1,23 @@
+const convertToMinute = (timeInHour) => {
+  const time = timeInHour.split(':');
+  const timeInMinyte = Number(time[0])*60 + Number(time[1]);
+  return timeInMinyte;
+}
+
+const workDay = (startTime, endTime, startMeeting, timeMeeting) => {
+  const startTimeMinute = convertToMinute(startTime);
+  const endTimeMinute = convertToMinute(endTime);
+  const startMeetingMinute = convertToMinute(startMeeting);
+
+  if (startMeetingMinute + timeMeeting > endTimeMinute || startMeetingMinute < startTimeMinute) {
+    return false;
+  }{
+    return true;
+  };
+}
+
+console.log(workDay('08:00', '17:30', '14:00', 90));
+console.log(workDay('14:00', '17:30', '08:0', 90));
+console.log(workDay('8:30', '17:30', '08:00', 40));
+console.log(workDay('08:00', '14:30', '14:00', 90));
+console.log(workDay('8:0', '10:0', '8:0', 120));
